9033f27465ba127ddf490e13f145f0029f9fe3bf
Jona <jona303@gmail.com>
2015-12-30 11:10:21 +0100

    Short translations templating

--- Controller/ShortTranslationController.php
+++ Controller/ShortTranslationController.php
@@ -3,11 +3,12 @@
 namespace SKCMS\LocaleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ShortTranslationController extends Controller
 {
-    public function indexAction($path = null,Request $request)
+    public function indexAction($forceJson=0,$path = null,Request $request)
     {
         $loader = $this->get('skcms_locale.translations.loader');
         $loader->loadTranslations();
@@ -16,11 +17,55 @@ class ShortTranslationController extends Controller
 
         if ($request->getMethod() == 'POST'){
             if($loader->bindForm($request)){
-                return $this->redirectToRoute($request->attributes->get('_route'));
+                if ($forceJson){
+                    return new JsonResponse(['status'=>1,'callBackParams'=>['refresh'=>1]]);
+                }
+                else{
+                    return $this->redirectToRoute($request->attributes->get('_route'));
+                }
+
             }
         }
 
-        return $this->render('SKCMSLocaleBundle:translations:translations.html.twig',['translations'=>$loader->getTranslationTree(),'form'=>$loader->getForm()->createView()]);
+        $renderParams = ['translations'=>$loader->getTranslationTree(),'form'=>$loader->getForm()->createView()];
+        $templateFile = 'SKCMSLocaleBundle:translations:translations.html.twig';
+        if ($forceJson){
+            return new JsonResponse(['status'=>1,'callBackParams'=>['view'=>$this->renderView($templateFile,$renderParams)]]);
+        }
+        else{
+            return $this->render($templateFile,$renderParams);
+        }
+
+
+    }
+    public function formAction($forceJson=0,$path = null,Request $request)
+    {
+        $loader = $this->get('skcms_locale.translations.loader');
+        $loader->loadTranslations();
+        $loader->createForm($path);
+
+
+        if ($request->getMethod() == 'POST'){
+            if($loader->bindForm($request)){
+                if ($forceJson){
+                    return new JsonResponse(['status'=>1,'callBackParams'=>['refresh'=>1]]);
+                }
+                else{
+                    return $this->redirectToRoute($request->attributes->get('_route'));
+                }
+
+            }
+        }
+
+        $renderParams = ['translations'=>$loader->getTranslationTree(),'form'=>$loader->getForm()->createView()];
+        $templateFile = 'SKCMSLocaleBundle:translations:form.html.twig';
+        if ($forceJson){
+            return new JsonResponse(['status'=>1,'callBackParams'=>['view'=>$this->renderView($templateFile,$renderParams)]]);
+        }
+        else{
+            return $this->render($templateFile,$renderParams);
+        }
+
 
     }
 }
