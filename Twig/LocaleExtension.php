83e083ee00cc54607075b9fe9f34751ced2d5d22
Jona <jona303@gmail.com>
2016-01-04 16:51:02 +0100

    Adding Locale filter

--- Twig/LocaleExtension.php
+++ Twig/LocaleExtension.php
@@ -9,17 +9,47 @@ use SKCMS\LocaleBundle\Loader\TranslationFilesLoader;
 
 class LocaleExtension extends \Twig_Extension
 {
+
+    private $container;
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('SKCMSLocaleTranslationDecode', array($this, 'SKCMSLocaleTranslationDecode'))
+            new \Twig_SimpleFilter('SKCMSLocaleTranslationDecode', array($this, 'SKCMSLocaleTranslationDecode')),
+            new \Twig_SimpleFilter('localeToName', array($this, 'localeToName'))
         ];
+    }
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('skcmsEnabledLocales', array($this, 'skcmsEnabledLocales'))
+        ];
+    }
+
+    public function skcmsEnabledLocales(){
+        return $this->container->getParameter('skcms.locale.locales');
     }
 
     public function SKCMSLocaleTranslationDecode($input){
         return TranslationFilesLoader::stringSanitizer($input,true);
     }
 
+
+    public function localeToName($locale){
+        $table = [
+            'fr'=>'Français',
+            'en'=>'English',
+            'de'=>'Deutsch',
+            'nl'=>'Nederlands',
+        ];
+
+            return isset($table[$locale])? $table[$locale] : $locale;
+
+    }
 
     public function getName()
     {
